26a9e940786560b1ec107289d92fb83e58b6cf05
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2021-07-02 20:11:09 +0300

    add login and sign up

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,12 +1,34 @@
 const Validations = require('../modules/validations.js')
 const {generateHash} = require('../modules/bcrypt.js')
 const {generateToken} = require('../modules/jwt.js')
-
+const {compareHash} = require('../modules/bcrypt.js')
 module.exports = class UserController{
 	static async UserPostLoginController(req , res){
-    res.json({
-    	ok:"server is working!"
-    })
+    try {
+     	const {username , password} = await Validations.UserSignUpValidation().validateAsync(req.body)
+    	const user = await req.db.users.findOne({
+    		where:{
+    			user_name:username
+    		}
+    	})
+       if(!user) throw new Error('User not found')
+       
+       if(!compareHash(password , user.dataValues.user_password)) throw new Error("Password is incorrect")
+        
+        res.status(201).json({
+          ok:true,
+          token: await generateToken(user.dataValues.user_id),
+          data:{
+          	name: username
+          }
+        })
+        console.log(user)  
+    } catch(e) {
+       res.status(401).json({
+       	ok: false,
+       	message: e + ""
+       })
+    }
 	}
 
 	static async UserPostSignUpController(req , res){
